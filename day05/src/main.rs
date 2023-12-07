38bd85f5cbcb32041c877b4b3f03d8dafe39504c
amfaber <andreas.mfaber@gmail.com>
2023-12-07 20:35:26 +0100

    day 7

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,12 +1,22 @@
-use std::ops::Range;
+use std::{
+    collections::HashSet,
+    ops::{Range, RangeInclusive},
+    time::Instant,
+};
 
 use shared::{get_day_input, lazy_input, Lazy};
-lazy_input!{5}
+lazy_input! {5}
 
-#[derive(Debug)]
-struct MapEntry{
-    input_range: Range<i64>,
+#[derive(Debug, Clone)]
+struct MapEntry {
+    input_range: RangeInclusive<i64>,
     output_delta: i64,
+}
+
+impl MapEntry {
+    fn apply(&self) -> RangeInclusive<i64> {
+        *self.input_range.start() + self.output_delta..=self.input_range.end() + self.output_delta
+    }
 }
 
 const _TEST_INPUT: &str = "seeds: 79 14 55 13
@@ -43,49 +53,205 @@ humidity-to-location map:
 60 56 37
 56 93 4";
 
-fn part1(){
+fn part1() {
     let mut categories = INPUT.split("\n\n");
-    // let mut categories = _TEST_INPUT.split("\n\n");
-    let seeds = categories.next().unwrap().split(" ").filter_map(|seed| seed.parse::<i64>().ok());
+    let seeds = categories
+        .next()
+        .unwrap()
+        .split(" ")
+        .filter_map(|seed| seed.parse::<i64>().ok());
 
-    let maps = categories.map(|category|{
-        let mut lines = category.lines();
-        lines.next();
-        let map = lines.map(|line| {
-            let mut elems = line.split(" ").map(|num| num.parse::<i64>().unwrap());
-            let dest_start = elems.next().unwrap();
-            let source_start = elems.next().unwrap();
-            let n = elems.next().unwrap();
-            MapEntry{
-                input_range: source_start..source_start + n,
-                output_delta: dest_start - source_start,
-            }
-        }).collect::<Vec<_>>();
-        // map.sort_by_key(|entry| entry.input_range.start);
-        // dbg!(&map);
-        map
-    }).collect::<Vec<_>>();
+    let maps = categories
+        .map(|category| {
+            let mut lines = category.lines();
+            lines.next();
+            let map = lines
+                .map(|line| {
+                    let mut elems = line.split(" ").map(|num| num.parse::<i64>().unwrap());
+                    let dest_start = elems.next().unwrap();
+                    let source_start = elems.next().unwrap();
+                    let n = elems.next().unwrap();
+                    MapEntry {
+                        input_range: source_start..=source_start + n - 1,
+                        output_delta: dest_start - source_start,
+                    }
+                })
+                .collect::<Vec<_>>();
+            map
+        })
+        .collect::<Vec<_>>();
 
-    let out = seeds.map(|mut input|{
-        for map in &maps{
-            for entry in map{
-                if entry.input_range.contains(&input){
-                    input += entry.output_delta;
-                    break
+    let out = seeds
+        .map(|mut input| {
+            for map in &maps {
+                for entry in map {
+                    if entry.input_range.contains(&input) {
+                        input += entry.output_delta;
+                        break;
+                    }
                 }
             }
-            // dbg!(input);
-            // dbg!(());
-            // dbg!(());
+            input
+        })
+        .min().unwrap();
+
+    dbg!(out);
+}
+
+fn range_contains_range(seeds: &RangeInclusive<i64>, input: &RangeInclusive<i64>) -> (bool, bool) {
+    // input.end
+    // seeds.end()
+    (seeds.contains(input.start()), seeds.contains(input.end()))
+}
+
+fn find_unused_rec(
+    to_search: RangeInclusive<i64>,
+    mut ranges: &[MapEntry],
+) -> (
+    Option<RangeInclusive<i64>>,
+    Option<(&[MapEntry], RangeInclusive<i64>)>,
+) {
+    let mut start_point = *to_search.start();
+    let end = *to_search.end();
+    loop {
+        if ranges.is_empty() {
+            return (Some(start_point..=end), None);
         }
-        // dbg!(input);
-        input
-    }).min();
-    
+        if start_point == *ranges[0].input_range.start() {
+            if end == *ranges[0].input_range.end() {
+                return (None, None);
+            }
+            start_point = *ranges[0].input_range.end() + 1;
+            ranges = &ranges[1..];
+        } else {
+            return (
+                Some(start_point..=*ranges[0].input_range.start() - 1),
+                Some((&ranges[1..], *ranges[0].input_range.start()..=end)),
+            );
+        }
+    }
+}
+
+fn find_unused(
+    mut to_search: RangeInclusive<i64>,
+    mut ranges: &[MapEntry],
+) -> Vec<RangeInclusive<i64>> {
+    let mut unused = Vec::new();
+    loop {
+        let (elem, new_ranges) = find_unused_rec(to_search, ranges);
+        if let Some(elem) = elem {
+            unused.push(elem);
+        }
+        match new_ranges {
+            Some((new_ranges, new_to_search)) => {
+                to_search = new_to_search;
+                ranges = new_ranges;
+            }
+            None => break,
+        }
+    }
+    unused
+}
+
+fn part2() {
+    let mut categories = INPUT.split("\n\n");
+    let seeds = categories
+        .next()
+        .unwrap()
+        .split(" ")
+        .filter_map(|seed| seed.parse::<i64>().ok())
+        .collect::<Vec<_>>();
+    let seeds_ranges = seeds.chunks_exact(2).map(|chunk| {
+        let &[start, n] = chunk else { panic!() };
+        vec![start..=start + n - 1]
+    });
+
+    let maps = categories
+        .map(|category| {
+            let mut lines = category.lines();
+            lines.next();
+            let map = lines
+                .map(|line| {
+                    let mut elems = line.split(" ").map(|num| num.parse::<i64>().unwrap());
+                    let dest_start = elems.next().unwrap();
+                    let source_start = elems.next().unwrap();
+                    let n = elems.next().unwrap();
+                    MapEntry {
+                        input_range: source_start..=source_start + n - 1,
+                        output_delta: dest_start - source_start,
+                    }
+                })
+                .collect::<Vec<_>>();
+            map
+        })
+        .collect::<Vec<_>>();
+
+    let mut i = 0;
+
+    let out = seeds_ranges
+        .map(|mut input| {
+            for map in maps.iter()
+            {
+                let mut new_input = Vec::new();
+                for range in input.iter() {
+                    let mut used = Vec::new();
+                    for entry in map {
+                        match range_contains_range(&range, &entry.input_range) {
+                            (true, true) => {
+                                used.push(
+                                    entry.clone(),
+                                );
+                            }
+                            (true, false) => {
+                                used.push(
+                                    MapEntry {
+                                        input_range: *entry.input_range.start()..=*range.end(),
+                                        output_delta: entry.output_delta,
+                                    },
+                                );
+                            }
+                            (false, true) => {
+                                used.push(
+                                    MapEntry {
+                                        input_range: *range.start()..=*entry.input_range.end(),
+                                        output_delta: entry.output_delta,
+                                    }
+                                );
+                            }
+                            (false, false) => {
+                                if range.start() > entry.input_range.start() && range.end() < entry.input_range.end(){
+                                    used.push(
+                                        MapEntry {
+                                            input_range: *range.start()..=*range.end(),
+                                            output_delta: entry.output_delta,
+                                        },
+                                    );
+                                }
+                            }
+                        }
+                    }
+                    used.sort_by_key(|range| *range.input_range.start());
+                    let unused = find_unused(range.clone(), &used);
+                    new_input.extend(unused);
+                    new_input.extend(used.into_iter().map(|entry| entry.apply()));
+                }
+                input = new_input;
+                i += 1;
+            }
+
+            input.into_iter().map(|range| *range.start()).min().unwrap()
+        })
+        .min().unwrap();
 
     dbg!(out);
 }
 
 fn main() {
+    Lazy::force(&INPUT);
+    let now = Instant::now();
     part1();
+    dbg!(now.elapsed());
+    let now = Instant::now();
+    part2();
+    dbg!(now.elapsed());
 }
--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,3 +1,105 @@
+use std::{collections::BTreeMap, cmp::Reverse, time::Instant};
+
+use shared::{get_day_input, lazy_input, Lazy};
+lazy_input! {7}
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
+struct Card(u8);
+
+#[derive(Clone, Copy)]
+enum Part{
+    One,
+    Two,
+}
+
+impl Card{
+    fn parse(c: char, part: Part) -> Self{
+        match c{
+            'T' => Self(10),
+            'J' => match part{
+                Part::One => Self(11),
+                Part::Two => Self(0),
+            },
+            'Q' => Self(12),
+            'K' => Self(13),
+            'A' => Self(14),
+            _ => Self(c.to_digit(10).unwrap() as u8),
+        }
+    }
+}
+
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
+struct Hand {
+    sorted_count: Vec<i32>,
+    cards: Vec<Card>,
+}
+
+impl Hand{
+    fn parse(s: &str, part: Part) -> Self{
+        let cards = s.chars().map(|c| Card::parse(c, part)).collect::<Vec<_>>();
+        let mut map = BTreeMap::new();
+        for card in &cards{
+            *map.entry(card).or_insert(0) += 1;
+        }
+        let sorted_count = match part{
+            Part::One => {
+                let mut count = map.into_iter().map(|(_, count)| count).collect::<Vec<_>>();
+                count.sort_unstable_by_key(|count| Reverse(*count));
+                count
+            },
+            Part::Two => {
+                let mut count = map.into_iter().collect::<Vec<_>>();
+                count.sort_unstable_by_key(|(_, count)| Reverse(*count));
+                let jack = Card::parse('J', Part::Two);
+                if let Some(idx) = count.iter().position(|(card, _)|{
+                    **card == jack
+                }){
+                    if count.len() > 1{
+                        let (_, jack_count) = count.remove(idx);
+                        count.first_mut().unwrap().1 += jack_count;
+                    }
+                }
+                count.into_iter().map(|(_, count)| count).collect::<Vec<_>>()
+            },
+        };
+        Self{
+            cards,
+            sorted_count,
+        }
+    }
+}
+
+const _TEST_INPUT: &str = "32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483";
+
+fn execute(part: Part){
+    // let mut games = _TEST_INPUT.lines().map(|line| {
+    let mut games = INPUT.lines().map(|line| {
+        let (hand, bid) = line.split_once(" ").unwrap();
+        let hand = Hand::parse(hand, part);
+        let bid = bid.parse::<usize>().unwrap();
+        (hand, bid)
+    }).collect::<Vec<_>>();
+
+    games.sort_unstable_by(|(hand1, _), (hand2, _)|{
+        hand1.cmp(hand2)
+    });
+
+    let out = games.into_iter().enumerate().map(|(rank, (_, bid))| (rank + 1) * bid).sum::<usize>();
+
+    dbg!(out);
+}
+
 fn main() {
-    println!("Hello, world!");
+    Lazy::force(&INPUT);
+    let now = Instant::now();
+    execute(Part::One);
+    dbg!(now.elapsed());
+    let now = Instant::now();
+    execute(Part::Two);
+    dbg!(now.elapsed());
 }
